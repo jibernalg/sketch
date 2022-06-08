575e1102a33a8401b0f606c7cbe33c478dfcddaa
Jorge Bernal <jibernalg@hotmail.com>
2022-06-08 16:29:36 -0500

    modificacion colores

--- javascript.js
+++ javascript.js
@@ -1,23 +1,21 @@
 function createPixels(size) {
-    let container = document.querySelector(".container");
-    for (i = 0; i < size * size; i++) {
-      let pixel = document.createElement("div");
-      pixel.setAttribute("class", "pixel");
-      pixel.addEventListener("pointermove", pintar);
-      container.appendChild(pixel);
-    }
+  let cuadritos = document.querySelector(".container");
+  for (i = 0; i < size * size; i++) {
+    let pixel = document.createElement("div");
+    pixel.setAttribute("class", "pixel");
+    pixel.addEventListener("pointermove", pintar);
+    cuadritos.appendChild(pixel);
   }
-  
-  createPixels(16);
-  
-  function pintar(e) {
-    e.target.style = `background-color: red`;
-    
+}  
+createPixels(16);
+ 
 
-    
-  }
-  
-  function resetContainer() {
-    let allPixels = document.querySelectorAll(".pixel");
-    allPixels.forEach(pixel => pixel.style=`background-color: white`);
-  }
+function pintar(e) {
+  e.target.style = `background-color: red`;
+}
+
+
+function resetContainer() {
+  let allPixels = document.querySelectorAll(".pixel");
+  allPixels.forEach(pixel => pixel.style=`background-color: white`);
+}
